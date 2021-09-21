86a8ce4adaea42f70b9574d052134a12b78dbbfa
frozar <fabien.rozar@gmail.com>
2021-09-21 05:09:18 +0400

    Implementation of the bonus version of the pyramide exercice.

--- basic_poo/01_pyramide/src/main/java/com/example/Application.java
+++ basic_poo/01_pyramide/src/main/java/com/example/Application.java
@@ -3,21 +3,26 @@ package com.example;
 public class Application {
   public static void main(String[] args) {
     int height = Integer.parseInt(args[0]);
+    int limit = Integer.parseInt(args[1]);
 
-    for (int i = 0; i < height; ++i) {
-      int lineWidth = height * 2 - 1;
-      int nbCross = lineWidth - 2 * i;
-      int nbSpace = i;
-      for (int j = 0; j < nbSpace; ++j) {
-        System.out.print(" ");
-      }
-      for (int j = 0; j < nbCross; ++j) {
-        System.out.print("*");
-      }
-      for (int j = 0; j < nbSpace; ++j) {
-        System.out.print(" ");
-      }
-      System.out.println("");
+    for (int i = 0; i < height - limit; ++i) {
+      printLine(height, i);
     }
+  }
+
+  private static void printLine(int height, int i) {
+    int lineWidth = height * 2 - 1;
+    int nbCross = lineWidth - 2 * i;
+    int nbSpace = i;
+    for (int j = 0; j < nbSpace; ++j) {
+      System.out.print(" ");
+    }
+    for (int j = 0; j < nbCross; ++j) {
+      System.out.print("*");
+    }
+    for (int j = 0; j < nbSpace; ++j) {
+      System.out.print(" ");
+    }
+    System.out.println("");
   }
 }
