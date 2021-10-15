7edf89398330d54d0e420a472759685087550722
frozar <fabien.rozar@gmail.com>
2021-10-15 13:06:36 +0400

    MAJ

--- 02_basic_spring_boot/06_shifumi/src/main/java/com/example/springboot/ShifumiController.java
+++ 02_basic_spring_boot/06_shifumi/src/main/java/com/example/springboot/ShifumiController.java
@@ -8,14 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ShifumiController {
 
+  // curl http://localhost:8080/?id=2
   @GetMapping("/")
   public String index(@RequestParam(name = "id") String idAction) {
-    return "Shifumi action: " + idAction;
+    return "1 Shifumi action: " + idAction;
   }
 
+  // curl http://localhost:8080/2
   @GetMapping("/{idAction}")
   public String pathVariable(@PathVariable String idAction) {
-    return "Shifumi action: " + idAction;
+    return "2 Shifumi action: " + idAction;
   }
 
 }
